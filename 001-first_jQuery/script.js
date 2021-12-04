816cd30eb833c27170f573b4e16a4ca39bb64f36
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-12-04 17:14:37 +0100

    Add files via upload

---
+++ 001-first_jQuery/script.js
@@ -0,0 +1,18 @@
+$(function () {
+    $("#start").html("Go!");
+});
+
+/*classic JavaScript code looked like:
+
+var el = document.getElementById("start");
+el.innerHTML = "Go!";
+#####################
+SYNTAX:
+The basic syntax of jQuery is
+$("selector").action()
+-The $ accesses jQuery;
+-The (selector) finds the HTML elements;
+-The action() is then performed on the element(s).
+*
+jQuery uses the CSS syntax.
+*/
